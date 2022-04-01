fa7440ed401c4fc057555a7565b1e124dfc01a0b
Kirill Saulyak <kerich345@gmail.com>
2022-04-02 01:08:00 +0300

    cicd

---
+++ src/test/java/com/example/cicd/FirstRestControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.cicd;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+class FirstRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void getFirst() throws Exception {
+        mockMvc.perform(get("/first"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("First page")));
+    }
+}
