8a9152b78d56f78616574ee1eeb2527c5915db1c
Kirill Saulyak <kerich345@gmail.com>
2022-04-15 15:01:01 +0300

    good

--- src/main/java/com/example/cicd/FirstRestController.java
+++ src/main/java/com/example/cicd/FirstRestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstRestController {
     @GetMapping("/first")
-    public Message getFirst()
+    public String getFirst()
     {
-        return new Message("First page.");
+        return "First page.";
     }
 }
